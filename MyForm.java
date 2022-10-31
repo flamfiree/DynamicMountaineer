3b1fa620edcd69190d456e99a9469f91f3935309
Anton Permyakov <permyakov.anton2815a@gmail.com>
2022-10-31 20:04:21 +0300

    A lot of new objects on form, createUIComponents() added, so... It goes on

--- MyForm.java
+++ MyForm.java
@@ -5,20 +5,38 @@ public class MyForm extends JFrame{
     private JButton restButton;
     private JLabel outLabel;
     private JPanel MyPanel;
+    private JSpinner speedSpin;
+    private JSlider maxPowerSlider;
+    private JComboBox lengthBox;
+    private JComboBox heightBox;
+    private JProgressBar powerBar;
+    private JLabel maxPowerLabel;
+    private JButton startButton;
 
-    Mountaineer mountaineer = new Mountaineer();
+    Mountaineer mountaineer;
+
+    private void start(){
+        mountaineer = new Mountaineer((int)speedSpin.getValue(),maxPowerSlider.getValue());
+        climbButton.setEnabled(true);
+    }
+
     private void climb(){
-        outLabel.setText(mountaineer.climb() + "    \n" + mountaineer.power);
-        if(mountaineer.power <= 0){
+        climbButton.setEnabled(false);
+        restButton.setEnabled(true);
+        outLabel.setText(mountaineer.climb() + "    " + mountaineer.getPower());
+        if(mountaineer.getPower() <= 0){
             this.rest();
         }
         timer.start();
     }
     private void rest(){
-        outLabel.setText(mountaineer.rest() + "     \n" + mountaineer.power);
+        climbButton.setEnabled(true);
+        restButton.setEnabled(false);
+        outLabel.setText(mountaineer.rest() + "     " + mountaineer.getPower());
     }
 
     private void action(){
+//        timer.setDelay(1000*(Hill.length/mountaineer.speed));
         if(mountaineer.getMountaineer().getClass() == RestMount.class){
             this.rest();
         }
@@ -32,12 +50,37 @@ public class MyForm extends JFrame{
     public MyForm(){
         this.setVisible(true);
         this.setContentPane(MyPanel);
-        this.setSize(200,100);
+        this.setSize(500,400);
+        Hill.length = 3000;
+        Hill.height = 1000;
+        climbButton.setEnabled(false);
+        restButton.setEnabled(false);
+        startButton.addActionListener(e -> start());
         climbButton.addActionListener(e -> climb());
         restButton.addActionListener(e -> rest());
+
     }
 
     public static void main(String[] args) {
         new MyForm();
+    }
+
+    private void createUIComponents() {
+        Integer[] length = new Integer[]{200,300,400,500,600,700,800};
+        lengthBox = new JComboBox<>(length);
+
+        Integer[] height = new Integer[]{1000,1500,2000,2500,3000};
+        heightBox = new JComboBox<>(height);
+
+        speedSpin = new JSpinner(new SpinnerNumberModel(1, 1,15,1));
+        ((JSpinner.DefaultEditor) speedSpin.getEditor()).getTextField().setEditable(false);
+
+        maxPowerSlider = new JSlider(10,100,50);
+        maxPowerSlider.setPaintTrack(true);
+        maxPowerSlider.setPaintTicks(true);
+        maxPowerSlider.setPaintLabels(true);
+        maxPowerSlider.setMajorTickSpacing(10);
+        maxPowerSlider.setMinorTickSpacing(5);
+        maxPowerSlider.addChangeListener( e -> maxPowerLabel.setText(" = " + maxPowerSlider.getValue()));
     }
 }
