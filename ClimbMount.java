d996204cfe87a8d28d94d34e2a1236bfa16d3d0b
Anton Permyakov <permyakov.anton2815a@gmail.com>
2022-11-01 17:22:40 +0300

    I broke the climbing process into three methods: climbUp(), climbPlain() and climbDown(). The  mountaineer has been moved to the class

--- ClimbMount.java
+++ ClimbMount.java
@@ -1,16 +1,49 @@
-public class ClimbMount implements IMountaineerState{
-    public ClimbMount(){
+import java.awt.geom.Point2D;
 
-    }
+public class ClimbMount extends State {
+    Mountaineer mountaineer;
+
     @Override
     public String climbing(Mountaineer mountaineer) {
-        mountaineer.setPower(mountaineer.getPower() - 1);
-        return "Still climbing";
+        this.mountaineer = mountaineer;
+        if(mountaineer.getPoint().x < (double) Hill.length/3){
+            climbUp();
+        }
+        else if(mountaineer.getPoint().x > (double) Hill.length/3 && mountaineer.getPoint().x <(double) 2*Hill.length/3){
+            climbPlain();
+        }
+        else if(mountaineer.getPoint().x > (double) 2*Hill.length/3 && mountaineer.getPoint().x < Hill.length){
+            climbDown();
+        } else
+            return "End.";
+        return "Climbing";
     }
-    @Override
-    public String resting(Mountaineer mountaineer) {
-        mountaineer.setMountaineer(new RestMount());
-        mountaineer.setPower(mountaineer.getPower() + 1);
-        return "Climb -> Rest";
+    private void climbUp(){
+        double tg = Hill.height/(double)(Hill.length/3);
+        double cos = (double) (Hill.length/3)
+                / (Math.sqrt(Math.pow(Hill.height,2) + Math.pow((double) Hill.length/3,2)));
+        mountaineer.setCurSpeed(mountaineer.getInitSpeed()*cos);
+        mountaineer.setPower(mountaineer.getPower() - 3);
+        mountaineer.setPoint(new Point2D.Double(
+                mountaineer.getPoint().x + mountaineer.getCurSpeed(),
+                mountaineer.getPoint().x * tg)
+        );
+
+    }
+    private void climbPlain(){
+        mountaineer.setCurSpeed(mountaineer.getInitSpeed());
+        mountaineer.setPower(mountaineer.getPower() - 2);
+        mountaineer.setPoint(new Point2D.Double(mountaineer.getPoint().x++,mountaineer.getPoint().y));
+    }
+    private void climbDown(){
+        double tg = Hill.height/(double)(Hill.length/3);
+        double cos = (double) (Hill.length/3)
+                / (Math.sqrt(Math.pow(Hill.height,2) + Math.pow((double) Hill.length/3,2)));
+        mountaineer.setCurSpeed(mountaineer.getInitSpeed()*(2 - cos));
+        mountaineer.setPower(mountaineer.getPower() - 1);
+        mountaineer.setPoint(new Point2D.Double(
+                mountaineer.getPoint().x++,
+                mountaineer.getPoint().y - tg)
+        );
     }
 }
