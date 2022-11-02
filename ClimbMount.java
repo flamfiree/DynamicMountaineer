602d3cb649e858239cb3d3deada31d9a00a75266
Anton Permyakov <permyakov.anton2815a@gmail.com>
2022-11-02 18:13:53 +0300

    Fixed coordinate logic.

--- ClimbMount.java
+++ ClimbMount.java
@@ -15,7 +15,7 @@ public class ClimbMount extends State {
         else if(mountaineer.getPoint().x > (double) 2*Hill.length/3 && mountaineer.getPoint().x < Hill.length){
             climbDown();
         } else
-            return "End.";
+            return "End";
         return "Climbing";
     }
     private void climbUp(){
@@ -26,14 +26,16 @@ public class ClimbMount extends State {
         mountaineer.setPower(mountaineer.getPower() - 3);
         mountaineer.setPoint(new Point2D.Double(
                 mountaineer.getPoint().x + mountaineer.getCurSpeed(),
-                mountaineer.getPoint().x * tg)
+                (mountaineer.getPoint().x + mountaineer.getCurSpeed()) * tg)
         );
 
     }
     private void climbPlain(){
         mountaineer.setCurSpeed(mountaineer.getInitSpeed());
         mountaineer.setPower(mountaineer.getPower() - 2);
-        mountaineer.setPoint(new Point2D.Double(mountaineer.getPoint().x++,mountaineer.getPoint().y));
+        mountaineer.setPoint(new Point2D.Double(
+                mountaineer.getPoint().x + mountaineer.getInitSpeed(),
+                mountaineer.getPoint().y));
     }
     private void climbDown(){
         double tg = Hill.height/(double)(Hill.length/3);
@@ -41,9 +43,13 @@ public class ClimbMount extends State {
                 / (Math.sqrt(Math.pow(Hill.height,2) + Math.pow((double) Hill.length/3,2)));
         mountaineer.setCurSpeed(mountaineer.getInitSpeed()*(2 - cos));
         mountaineer.setPower(mountaineer.getPower() - 1);
+//        mountaineer.setPoint(new Point2D.Double(
+//                mountaineer.getPoint().x + mountaineer.getCurSpeed(),
+//                mountaineer.getPoint().y - tg)
+//        );
         mountaineer.setPoint(new Point2D.Double(
-                mountaineer.getPoint().x++,
-                mountaineer.getPoint().y - tg)
+                mountaineer.getPoint().x + mountaineer.getCurSpeed(),
+                - tg * ((mountaineer.getPoint().x + mountaineer.getCurSpeed()) - Hill.length))
         );
     }
 }
